2ad1e463b351b77428f97c2d03fbac84a393baf4
Marco Carrano <mrc.carrano@gmail.com>
2022-08-20 17:43:31 +0200

    html

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def user(name):
 def page_not_found(e):
 	return render_template("404.html"), 404
 
-# Internal Server Error URL
+# Internal Server Error 
 
 @app.errorhandler(500)
 def page_not_found(e):
